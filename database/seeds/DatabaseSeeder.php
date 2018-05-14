0402cefa7f56df03593dfcdd6e913ba1c189ac91
Damian <damianudl@gmail.com>
2018-05-15 00:22:49 +0200

    seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(RolesTableSeeder::class);
-//
+//        $this->call(RolesTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+
 //        factory(App\User::class, 100)->create();
-//
 //        factory(App\Category::class, 10)->create();
 //        factory(App\Publisher::class, 20)->create();
-//
-        factory(App\Book::class, 100)->create();
+//        factory(App\Book::class, 100)->create();
     }
 }
